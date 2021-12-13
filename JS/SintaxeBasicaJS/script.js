ba1f0cf8b1883429fda075d8a6014ee21d429cb7
Willy Biti <willy.biti@gmail.com>
2021-12-13 20:27:30 -0300

    primeiro commit

---
+++ JS/SintaxeBasicaJS/script.js
@@ -0,0 +1,45 @@
+// let x = false;
+
+// console.log(typeof(x));
+
+//OPERADOR LOGICO
+
+//&& = E
+// II - OU
+// ! = NÃO
+
+// var adicao = 7%2;
+// console.log(adicao)
+
+// VETORES OU ARRAYS
+// let array = ['string', 1, true, ['array1'], ['array2']];
+
+// foeEach() = itera um array
+
+// array.forEach(function(ietm, índex){console.log(array)});
+
+// push(); = add um item no final do array
+
+// array.push('willy');
+// console.log(array)
+
+// pop(); = remove item no final do array 
+
+// shift(); = reove um item no inicio de um array
+
+// unshift(); add um item no inicio de um array
+
+// indexOf(); = retorna o indíce de um valor 
+
+//OBJETOS
+
+// var xicara = {
+//     cor: "azul",
+//     tamanho: "médio",
+//     numero : 1,
+// }
+
+// console.log(xicara.cor)
+
+//ESTRUTURAS CONDICIONAIS
+
