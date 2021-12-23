ae4deebd40b054283df876adc5639fcf08f55dd2
Willy Biti <willy.biti@gmail.com>
2021-12-23 13:13:16 -0300

    Primeiro commit SQL

--- SQL_S/AULA1.sql
+++ SQL_S/AULA1.sql
@@ -1,1 +1,15 @@
 Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
+
+create database Ecomerce //Criar base de dado
+
+use Ecomerce // Se direcionar pra uma base de dados
+
+int
+bigint
+varchar
+char
+bit 0, 1
+
+
+insert Teste values('a', 'a')
+select * from Teste
---
+++ SQL_S/SQLQuery1.sql
@@ -0,0 +1,4 @@
+select getdate()
+
+select * from sys.tables
+
