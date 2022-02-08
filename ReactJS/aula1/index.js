7a98aeb3871f8d8f32392b9e4453cb6902d09c1a
Willy Biti <willy.biti@gmail.com>
2022-02-08 12:55:50 -0300

    React introduction

---
+++ ReactJS/aula1/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDom from "react-dom"
+import "./src/style.css"
+
+// const App = () => {
+//     return{
+//         <div className='App'>
+//             Hallo
+//         </div>
+//     }
+// }
+
+const rootElemnt = document.getElementById('root')
+ReactDOM.render(<App/>, rootElemnt)
